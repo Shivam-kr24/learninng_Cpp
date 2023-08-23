bce27c262f39580a329ff57d5bc5b2b385d0070f
shivam k_singh <95178537+Shivam-kr24@users.noreply.github.com>
2023-08-23 17:41:08 +0530

    Add files via upload
    implemented OOPS concept in C++

---
+++ 27.friend class,member.cpp
@@ -0,0 +1,80 @@
+class Y;
+
+class X{
+    int data;
+    public:
+        void setValue(int value){
+            data = value;
+        }
+    friend void add(X, Y);    
+};
+
+class Y{
+    int num;
+    public:
+        void setValue(int value){
+            num = value;
+        }
+    friend void add(X, Y);    
+
+};
+
+void add(X o1, Y o2){
+    cout<<"Summing data of X and Y objects gives me "<< o1.data + o2.num;
+}
+int main(){
+    X a1;
+    a1.setValue(3);
+
+    Y b1;
+    b1.setValue(15);
+
+    add(a1, b1);
+    return 0;
+}
+class c2;
+
+class c1{
+    int val1;
+    friend void exchange(c1 & , c2 &);
+    public:
+        void indata(int a){
+            val1 = a;
+        }
+
+        void display(void){
+            cout<< val1 <<endl;
+        }
+};
+
+class c2{
+    int val2;
+    friend void exchange(c1 &, c2 &);
+    public:
+        void indata(int a){
+            val2 = a;
+        }
+
+        void display(void){
+            cout<< val2 <<endl;
+        }
+};
+
+void exchange(c1 &x, c2 &y){
+    int tmp = x.val1;
+    x.val1 = y.val2;
+    y.val2 = tmp;
+}
+
+int main()
+{
+    Complex o1, o2;
+    o1.setNumber(1, 4);
+    o2.setNumber(5, 7);
+    Calculator calc;
+    int res = calc.sumRealComplex(o1, o2);
+    cout << "The sum of real part of o1 and o2 is " << res << endl;
+    int resc = calc.sumCompComplex(o1, o2);
+    cout << "The sum of complex part of o1 and o2 is " << resc << endl;
+    return 0;
+}
